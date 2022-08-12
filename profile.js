5a3f726a71835c9f19da2d6c1219c7dd4032a95e
Taha Manjoura <t.manjoura@gmail.com>
2022-08-12 13:30:31 -0500

    htmlAssessment

---
+++ profile.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+let favCol = document.querySelector('#color')
+favCol.addEventListener('click', () => {
+alert('My favorite color is Blue') })
+
+let favPlace = document.querySelector('#place' )
+place.addEventListener('click', () => {
+alert('My favorite place is Miami') })
+
+
+let favRitual = document.querySelector('#ritual' )
+favRitual.addEventListener('click', () => {
+alert('My favorite ritual is spending time with family!') })
+
+
+
+
+
